ddf113cb95aa37d19c41fed12ee8ce6a3fd9466b
coystark <chcp.caio@gmail.com>
2022-05-02 18:25:30 -0300

    remove comments from getSimpleJwtAuth

--- src/core/auths.tsx
+++ src/core/auths.tsx
@@ -1,11 +1,11 @@
-// import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AxiosRequestConfig } from "axios";
-// import { get } from "lodash";
+import { get } from "lodash";
 
 export const getSimpleJwtAuth = (key: string, path: string) => {
   return {
     interceptorRequest: async (config: AxiosRequestConfig) => {
-      /* const authCached = await AsyncStorage.getItem(key);
+      const authCached = await AsyncStorage.getItem(key);
 
       if (authCached && config.headers) {
         const cachedParsed = JSON.parse(authCached);
@@ -13,7 +13,7 @@ export const getSimpleJwtAuth = (key: string, path: string) => {
         const token = get(cachedParsed, path);
 
         config.headers.Authorization = `Bearer ${token}`;
-      }*/
+      }
 
       return config;
     },
